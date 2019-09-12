f9e56e2fb17828bec5d779472a8407c6d48c34ce
allenfrostline <alvameer@gmail.com>
2019-09-11 19:28:18 -0500

    rebuild and deploy Wed Sep 11 19:28:18 CDT 2019

--- js/load-typekit.js
+++ js/load-typekit.js
@@ -5,13 +5,11 @@
   var text = "abcdefghijklmnopqrstuvwxyz0123456789";
   context.font = "72px monospace";
   var size = context.measureText(text).width;
-  var fonts = ['Source Han Serif', 'Adobe Kaiti'];
+  // songti
   var types = [' SC', ' CN', ' TC', ' TW', ''];
   for (var i = 0; i < types.length; i++) {
-    for (var j = 0; j < fonts.length; j++) {
-      context.font = "72px '" + fonts[j] + " " + types[i] + "', monospace";
-      if (context.measureText(text).width != size) return;
-    }
+    context.font = "72px '" + 'Source Han Serif' + types[i] + "', monospace";
+    if (context.measureText(text).width != size) return;
   }
   var config = {kitId: 'kwz5xar', scriptTimeout: 3000, async: true},
   h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
